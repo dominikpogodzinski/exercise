597be899df2c4f72772ae3bc9ffb6526965616b1
Dominik Pogodziński <dominikpogodzinski@wp.pl>
2019-11-07 15:32:48 +0100

    format language

--- generator_obelg_py.py
+++ generator_obelg_py.py
@@ -1,16 +1,16 @@
 from random import randint
 
-czesci_ciala = ["twarz", "noga", "ręka", "pięta"]
-przymiotniki = ['brzydsza', 'bardziej obsceniczna', 'szkaradniejsza', 'bardziej brudna']
-zwierzeta = ['dzikiej kuny', 'czupakabry', 'dzikiego węża', 'bobra']
+body_parts = ["twarz", "noga", "ręka", "pięta"]
+adjectives = ['brzydsza', 'bardziej obsceniczna', 'szkaradniejsza', 'bardziej brudna']
+animals = ['dzikiej kuny', 'czupakabry', 'dzikiego węża', 'bobra']
 
 
 def losuj(lista):
     return lista[randint(0, len(lista) - 1)]
 
 print('Twoja {} jest {} niż {} {}'.format(
-    losuj(czesci_ciala),
-    losuj(przymiotniki),
-    losuj(czesci_ciala),
-    losuj(zwierzeta)
+    losuj(body_parts),
+    losuj(adjectives),
+    losuj(body_parts),
+    losuj(animals)
 ))
